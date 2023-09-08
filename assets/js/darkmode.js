15aa9484b926c845796994f17ea5ff9d447b92af
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2023-09-08 08:50:17 +0200

    Make dark mode default

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,14 +1,15 @@
 var body = document.body;
 var switcher = document.getElementsByClassName('js-toggle')[0];
+switcher.classList.toggle('js-toggle--checked');
 
-//Click on dark mode icon. Add dark mode classes and wrappers. Store user preference through sessions
+// Click on dark mode icon. Add dark mode classes and wrappers. Store user preference through sessions
 switcher.addEventListener("click", function () {
     this.classList.toggle('js-toggle--checked');
     this.classList.add('js-toggle--focus');
-    //If dark mode is selected
+    // If dark mode is selected
     if (this.classList.contains('js-toggle--checked')) {
         body.classList.add('dark');
-        //Save user preference in storage
+        // Save user preference in storage
         localStorage.setItem('darkMode', 'true');
     } else {
         body.classList.remove('dark');
@@ -18,7 +19,7 @@ switcher.addEventListener("click", function () {
     }
 })
 
-//Check Storage. Keep user preference on page reload
+// Check Storage. Keep user preference on page reload
 if (localStorage.getItem('darkMode')) {
     switcher.classList.add('js-toggle--checked');
     body.classList.add('dark');
